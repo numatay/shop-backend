0b65fdee1543ff9da34b852f4d65a9b6fac3c593
Уматай Нургиса <nurgissa_umatay@epam.com>
2020-11-01 23:58:32 +0600

    feat: Add 2 methods for products service

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -1,17 +1,7 @@
-// import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
 import { getAllProducts } from './src/getAllProducts';
 import { getProductById } from "./src/getProductById";
 
-// export const hello: APIGatewayProxyHandler = async (event, _context) => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-//       input: event,
-//     }, null, 2),
-//   };
-// }
+import 'source-map-support/register';
 
 export {
   getAllProducts,
